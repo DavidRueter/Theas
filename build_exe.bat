ac34df59491d1e61c7311dc51942b8d86f6f4469
DavidRueter <drueter@assyst.com>
2016-08-11 08:03:13 -0700

    Tidy up the repository
    Remove some unneeded files, change a path in build_exe.bat

--- build_exe.bat
+++ build_exe.bat
@@ -1,6 +1,6 @@
-CALL \Projects\TheasServer\venv_py34_theas\scripts\activate.bat
-del \Projects\TheasServer\dist\*.*
-cd \Projects\TheasServer
+CALL \Projects\Theas\venv_py34_theas\scripts\activate.bat
+del \Projects\Theas\dist\*.*
+cd \Projects\Theas
 python setup.py py2exe
-cd \Projects\TheasServer\dist
+cd \Projects\Theas\dist
 PAUSE
