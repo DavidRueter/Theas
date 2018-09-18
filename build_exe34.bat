96d5f9a253185e94a5ce78a030dd9d392b0ab02b
DavidRueter <drueter@assyst.com>
2018-09-18 12:35:11 -0700

    Enhancements for vue.js
    Add MIME type extension mapping for .vue (single-file Vue components), filter arguments for theas_values_json and theas_resource to assist when using to render to JavaScript, argument for set_value to more easily control inclusion or exclusion from parameters sent to JavaScript.

--- build_exe34.bat
+++ build_exe34.bat
@@ -1,4 +1,4 @@
-CALL \Projects\Theas\venv_py36_theas\scripts\activate.bat
+CALL \Projects\Theas\venv_py34_theas\scripts\activate.bat
 del \Projects\Theas\dist\*.*
 cd \Projects\Theas
 python setup.py py2exe
